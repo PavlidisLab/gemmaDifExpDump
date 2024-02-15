da3c02a542429cc5e185c8627f8fea1a6f3efeee
OganM <ogan.mancarci@gmail.com>
2024-02-15 14:33:59 -0800

    compile code

---
+++ R/filepaths.R
@@ -0,0 +1,7 @@
+DATADIR <- '/cosmos/data/project-data/GemmaDifExp'
+
+FREEZEDIR <- '/cosmos/data/project-data/GemmaDifExp/gemma_freeze'
+
+RAWDIR <- "/cosmos/data/project-data/GemmaDifExp/raw_data"
+
+CACHEDIR <- '/space/scratch/Ogdata/GemmaDifExp/gemma_cache'
