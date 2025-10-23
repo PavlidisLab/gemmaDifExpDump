f6482b67ca4a95611d6dc904ba2e8288b6250558
OganM <ogan.mancarci@gmail.com>
2025-10-23 13:25:24 -0700

    update version

--- R/filepaths.R
+++ R/filepaths.R
@@ -1,7 +1,7 @@
-DATADIR <- '/cosmos/data/project-data/GemmaDifExp'
+DATADIR <- '/cosmos/data/project-data/GemmaDifExp/10.2025'
 
-FREEZEDIR <- '/cosmos/data/project-data/GemmaDifExp/gemma_freeze'
+FREEZEDIR <- '/cosmos/data/project-data/GemmaDifExp/10.2025/gemma_freeze'
 
-RAWDIR <- "/cosmos/data/project-data/GemmaDifExp/raw_data"
+RAWDIR <- "/cosmos/data/project-data/GemmaDifExp/10.2025/raw_data"
 
-CACHEDIR <- '/space/scratch/Ogdata/GemmaDifExp/gemma_cache'
+CACHEDIR <- '/space/scratch/Ogdata/GemmaDifExp/10.2025/gemma_cache'
